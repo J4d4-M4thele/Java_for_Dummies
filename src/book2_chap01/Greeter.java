b2c55d88b9bda9b61b183795f42dfcfec6513f16
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-02 17:23:21 +0200

    first commit

---
+++ src/book2_chap01/Greeter.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book2_chap01;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jada
+ */
+public class Greeter {
+//**********PRINTS IN  CONSOLE*********
+//    public void sayHello() {
+//        System.out.println("Hello World!");
+//    }
+  
+//**********ALERTS*********    
+    public void sayHello() {
+    JOptionPane.showMessageDialog(null, "Hello World!", "Greeter", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
---
+++ src/book2_chap01/HelloApp2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book2_chap01;
+
+/**
+ *
+ * @author Jada
+ */
+public class HelloApp2 {
+    public static void main(String[] args) {
+        Greeter greet1 = new Greeter();
+        greet1.sayHello();
+    }
+}
