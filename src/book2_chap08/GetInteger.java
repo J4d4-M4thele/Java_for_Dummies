521525c50b9ba0e8a323bf5925f2b7196733b957
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-04 14:41:32 +0200

    first commit

---
+++ src/book2_chap08/GetInteger.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book2_chap08;
+
+import java.util.*;
+
+/**
+ *
+ * @author Jada
+ */
+public class GetInteger {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter an integer: ");
+        int i = getAnInt();
+        System.out.println("You entered " + i);
+    }
+
+    public static int getAnInt() {
+        while (true) {
+            try {
+                return sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.next();
+                System.out.println("That's not an integer. Try again: ");
+            }
+        }
+    }
+}
