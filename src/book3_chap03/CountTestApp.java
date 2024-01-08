ba92784555d59adce3db7b6de86a752e6711b2c1
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-08 09:18:10 +0200

    update

--- src/book3_chap03/CountTestApp.java
+++ src/book3_chap03/CountTestApp.java
@@ -9,5 +9,31 @@ package book3_chap03;
  * @author Jada
  */
 public class CountTestApp {
-    
+
+    public static void main(String[] args) {
+        printCount();
+        for (int i = 0; i < 10; i++) {
+            //instantiating object
+            CountTest c1 = new CountTest();
+            printCount();
+        }
+    }
+
+    private static void printCount() {
+        System.out.println("There are now "
+                + CountTest.getInstanceCount() + " instances of the CountTest class.");
+    }
+}
+
+class CountTest {
+
+    private static int instanceCount = 0;
+    //constructor
+    public CountTest() {
+        instanceCount++;
+    }
+    //method
+    public static int getInstanceCount() {
+        return instanceCount;
+    }
 }
