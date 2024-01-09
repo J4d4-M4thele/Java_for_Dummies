32021d99b3fc33634d5f37f72c62cbb1f71f36e2
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-09 10:20:39 +0200

    first commit

---
+++ src/book1_chap03/HelloApp.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book1_chap03;
+
+/**
+ *
+ * @author Jada
+ */
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
