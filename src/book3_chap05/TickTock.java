835327d8030d8e9c0064df43e9372000069d958c
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-09 09:08:01 +0200

    update

--- src/book3_chap05/TickTock.java
+++ src/book3_chap05/TickTock.java
@@ -4,10 +4,37 @@
  */
 package book3_chap05;
 
+import java.awt.event.*;
+import javax.swing.*;
+
 /**
  *
  * @author Jada
  */
 public class TickTock {
-    
+
+    public static void main(String[] args) {
+        //event occurs every second (1st parameter)
+        //actioListenerInterface is extended (2nd parameter)
+        Timer t = new Timer(1000, new Ticker());
+        t.start();
+        //displaying a message (alert)
+        JOptionPane.showMessageDialog(null, "Click OK to exit program");
+    }
+}
+//interface implemented
+
+class Ticker implements ActionListener {
+
+    private boolean tick = true;
+
+    public void actionPerformed(ActionEvent event) {
+        if (tick) {
+            System.out.println("Tick...");
+        } else {
+            System.out.println("Tock...");
+        }
+        //toggles !tick means tick is now false
+        tick = !tick;
+    }
 }
