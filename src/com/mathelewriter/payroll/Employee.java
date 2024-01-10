b967359a5d787077e55db82e288bbc09a6a5491e
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-10 11:11:03 +0200

    update

--- src/com/mathelewriter/payroll/Employee.java
+++ src/com/mathelewriter/payroll/Employee.java
@@ -6,8 +6,75 @@ package com.mathelewriter.payroll;
 
 /**
  *
- * @author Jada 
+ * @author Jada Mathele
+ * @author www.MatheleWriter.com
+ * @version 1.5
+ * @since 1.0
  */
 public class Employee {
+
+    private String lastName;
+    private String firstName;
+    private Double salary;
+
+    /**
+     * Represents employee's address
+     */
+    public Address address;
+
+    /**
+     * Creates an employee with the specified name.
+     *
+     * @param lastName The employee's last name.
+     * @param firstName The employee's first name.
+     */
+    public Employee(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = new Address();
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the salary
+     */
+    public Double getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+    
     
 }
