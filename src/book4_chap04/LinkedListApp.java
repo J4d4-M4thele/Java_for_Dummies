61a94b73921801ca3775f419f6c685fa5421cc5a
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-01-11 12:51:09 +0200

    first commit

---
+++ src/book4_chap04/LinkedListApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book4_chap04;
+
+import java.util.LinkedList;
+/**
+ *
+ * @author Jada
+ */
+public class LinkedListApp {
+    public static void main(String[] args) {
+        
+    }
+}
